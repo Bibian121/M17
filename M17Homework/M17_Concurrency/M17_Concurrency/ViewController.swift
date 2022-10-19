bbc0c721e4481719982986b9e594d8622552db16
Bibian121 <bibian12@list.ru>
2022-10-19 23:37:48 +0300

    Commit GCD

--- M17Homework/M17_Concurrency/M17_Concurrency/ViewController.swift
+++ M17Homework/M17_Concurrency/M17_Concurrency/ViewController.swift
@@ -6,8 +6,19 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private var arrayURLs = [
+    "https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg",
+    "https://st.depositphotos.com/2547675/3009/i/450/depositphotos_30094505-stock-photo-time-clock.jpg",
+    "https://s1.1zoom.ru/big0/52/Love_Sunrises_and_sunsets_Fingers_Hands_Heart_Sun_532758_1280x897.jpg",
+    "https://oboi-telefon.ru/wallpapers/10232/39615.jpg",
+    "https://images.wallpaperscraft.ru/image/single/kanada_britanskaya_kolumbiya_gory_ozero_100429_225x300.jpg"
+    ]
+    
+    private var images = [Data]()
     
     let service = Service()
     
@@ -15,6 +26,14 @@ class ViewController: UIViewController {
         let view = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
         view.contentMode = .scaleAspectFit
         return view
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.spacing = 20
+        return stackView
     }()
     
     private lazy var activityIndicator: UIActivityIndicatorView = {
@@ -25,25 +44,71 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(imageView)
-        view.addSubview(activityIndicator)
+        view.addSubview(stackView)
+        stackView.addSubview(imageView)
+        stackView.addArrangedSubview(activityIndicator)
         activityIndicator.startAnimating()
+        setupViews()
         onLoad()
+    }
+    
+    private func setupViews() {
+        stackView.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottomMargin)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.topMargin)
+            make.left.right.equalToSuperview()
+        }
     }
 
     private func onLoad() {
-        service.getImageURL { urlString, error in
-            guard
-                let urlString = urlString
-            else {
-                return
+        let group = DispatchGroup()
+        for i in 0...4 {
+            group.enter()
+            asyncLoad(imageURL: URL(string: arrayURLs[i])!,
+                      runQueue: DispatchQueue.global(),
+                      completionQueue: DispatchQueue.main)
+            { result, error in
+                guard let image1 = result else {return}
+                print("---finished \(i) prioritet = \(qos_class_self().rawValue)")
+                self.images.append(image1)
+                group.leave()
             }
-            
-            let image = self.service.loadImage(urlString: urlString)
-            self.imageView.image = image
-            
+        }
+        
+        group.notify(queue: DispatchQueue.main) { [weak self] in
+            guard let self = self else { return }
             self.activityIndicator.stopAnimating()
+            self.stackView.removeArrangedSubview(self.activityIndicator)
+            for i in 0...4 {
+                self.addImage(data: self.images[i])
+            }
         }
     }
+}
+
+private extension ViewController {
+    func asyncLoad(
+        imageURL: URL,
+        runQueue: DispatchQueue,
+        completionQueue: DispatchQueue,
+        completion: @escaping (Data?, Error?) -> ()
+    ) {
+        runQueue.async {
+            do {
+                let data = try Data(contentsOf: imageURL)
+                completionQueue.async { completion(data, nil)}
+            } catch let error {
+                completionQueue.async { completion(nil, error)}
+            }
+        }
+    }
+    
+    func addImage(data: Data) {
+        let view = UIImageView(image: UIImage(data: data))
+        view.contentMode = .scaleAspectFit
+        self.stackView.addArrangedSubview(view)
+    }
+    
+    
 }
 
